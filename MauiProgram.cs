f7187100a6759dd761dc032adcf9efc5e528f76d
Divy Shah <69171707+DivyShah459@users.noreply.github.com>
2024-03-26 16:37:45 -0400

    Add files via upload

---
+++ MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Maui;
+using Blazored.Modal;
+
+namespace EdUnity;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			})
+			.UseMauiCommunityToolkit();
+		string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "edunity.db3");
+        builder.Services.AddSingleton<EdUnityRepository>(s => ActivatorUtilities.CreateInstance<EdUnityRepository>(s, dbPath));
+		builder.Services.AddMauiBlazorWebView();
+		builder.Services.AddBlazoredModal();
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		return builder.Build();
+	}
+}
